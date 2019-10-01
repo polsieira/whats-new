0132defb555069ea6bf8cd5b9a73a6c6ca695c06
posi7790 <posi7790@colorado.edu>
2019-10-01 13:15:42 -0600

    Add functional component and return JSX for a container full of news articles

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,25 @@
 import React from 'react';
-import './NewsContainer.css'
+import './NewsContainer.css';
+import NewsArticle from '../NewsArticle/NewsArticle';
 
-// NEWSCONTAINER COMPONENT CODE GOES HERE
+const NewsContainer = ({ local }) => {
+  const newsArticles = local.map(article => {
+    return (
+      <NewsArticle
+        key={article.id}
+        headline={article.headline}
+        description={article.description}
+        url={article.url}
+        img={article.img}
+      />
+    )
+  })
+
+  return (
+    <div className='newsArticle-container'>
+      {newsArticles}
+    </div>
+  )
+}
 
 export default NewsContainer;
