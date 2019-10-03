352509af95208070c66ca19e15e32f7bc351e447
posi7790 <posi7790@colorado.edu>
2019-10-03 09:55:57 -0600

    Add snapshot test for App

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { shallow } from "enzyme";
 import ReactDOM from 'react-dom';
 import App from './App';
 
@@ -6,4 +7,13 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+
+describe('App', () => {
+  it('should match the snapshot with all data passed in correctly', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
 });
