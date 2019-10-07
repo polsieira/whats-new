2ace4b28be530ba54ba831795cc49dd8bd5e4a9e
posi7790 <posi7790@colorado.edu>
2019-10-06 18:05:48 -0600

    Add icons

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { IoIosPin } from "react-icons/io";
+import { IoMdCog } from "react-icons/io";
+import { IoIosMusicalNotes } from "react-icons/io";
+import { IoIosFlask } from "react-icons/io";
+import { IoIosMedkit } from "react-icons/io";
+
 import './Menu.scss';
 
 const Menu = ({ changeState }) => {
@@ -7,11 +12,11 @@ const Menu = ({ changeState }) => {
     <aside className='Menu'>
       <h1 className="header">What's <span className="header--yellow">New?</span></h1>
       <ul>
-        <li id="local" onClick={(event) => changeState(event)}><IoIosPin size={30} />Local News</li>
-        <li id="technology" onClick={(event) => changeState(event)}>Technology</li>
-        <li id="entertainment" onClick={(event) => changeState(event)}>Entertainment</li>
-        <li id="science" onClick={(event) => changeState(event)}>Science</li>
-        <li id="health" onClick={(event) => changeState(event)}> Health</li>
+        <li id="local" onClick={(event) => changeState(event)}><IoIosPin size={30} /> Local News</li>
+        <li id="technology" onClick={(event) => changeState(event)}><IoMdCog size={30} /> Technology</li>
+        <li id="entertainment" onClick={(event) => changeState(event)}><IoIosMusicalNotes size={30} /> Entertainment</li>
+        <li id="science" onClick={(event) => changeState(event)}><IoIosFlask size={30} /> Science</li>
+        <li id="health" onClick={(event) => changeState(event)}><IoIosMedkit size={30} /> Health</li>
       </ul>
     </aside >
   )
